7a834bed7e5af834ed863b113ce2cd357533b282
nguyenrh4 <112020099+nguyenrh4@users.noreply.github.com>
2022-08-24 17:39:28 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/***************************************************
+ *Hello.java
+ ***************************************************
+ *Ryan Nguyen
+ *8/24/2022
+ *CMSC-255-001
+****************************************************/
+ 
+ public class Hello {
+ 	public static void main(String[] args) {
+ 
+ 		System.out.println("Ryan Nguyen");
+ 		System.out.println("macOS x64");
+ 		System.out.println("Hello.Java");
+ 		System.out.println("8/24/2022");
+ 		System.out.println("CMSC-255-001");
+ 		System.out.println();
+
+ 	}
+}
